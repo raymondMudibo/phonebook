e969b37fbbaa1bab66acca4d97c8e3ce64d5bc88
Raymond Mudibo <mudiboe@gmail.com>
2022-10-31 01:40:28 +0300

    implemented multiple delete

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php include './configuration/database.php';
 
+if (isset($_POST['multiple_delete'])) {
+    if (isset($_POST['deleteId'])) {
+        foreach ($_POST['deleteId'] as $id) {
+            $stmt = $pdo->prepare('DELETE FROM contacts WHERE id = :id');
+            $stmt->bindValue(':id', $id);
+            $stmt->execute();
+        }
+    }
+}
+
+
 $search = $_GET['search'] ?? '';
 if ($search) {
     $statement = $pdo->prepare('SELECT * FROM contacts WHERE first_name LIKE :search OR last_name LIKE :search 
@@ -11,8 +22,8 @@ if ($search) {
 }
 $statement->execute();
 $contacts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
+
 <?php require './partials/header.php' ?>
 
 
@@ -32,43 +43,69 @@ $contacts = $statement->fetchAll(PDO::FETCH_ASSOC);
     </div>
 </form>
 
-<table class="table">
-    <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Image</th>
-            <th scope="col">First Name</th>
-            <th scope="col">Last Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Phone Number</th>
-            <th scope="col">Action</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($contacts as $i => $contact) { ?>
-
+<form action="index.php" method="post">
+    <button type="submit" class="btn btn-sm btn-danger" name="multiple_delete" onclick="return confirm('Are you sure?')">
+        Delete
+    </button>
+    <table class="table">
+        <thead>
             <tr>
-                <th scope="row"><?php echo $i + 1 ?></th>
-                <td>
-                    <img src="<?php echo $contact['image'] ?>" height="50" width="50" />
-                </td>
-                <td><?php echo $contact['first_name'] ?></td>
-                <td><?php echo $contact['last_name'] ?></td>
-                <td><?php echo $contact['email'] ?></td>
-                <td><?php echo $contact['phone_number'] ?></td>
-                <td>
-                    <a href="view.php?id=<?php echo $contact['id'] ?>" type="button" class="btn btn-sm btn-warning">view</a>
-                    <a href="update.php?id=<?php echo $contact['id'] ?>" type="button" class="btn btn-sm btn-primary">Edit</a>
-
-                    <form method="post" action="delete.php" style="display:inline-block">
-                        <input type="hidden" name="id" value="<?php echo $contact['id'] ?>">
-                        <button type="submit" class="btn btn-sm btn-danger">Delete</button>
-                    </form>
-                </td>
+                <th scope="col" class="d-flex justify-content-center">
+                    <input type="checkbox" id="checkBoxAll" />
+                </th>
+                <th scope="col">#</th>
+                <th scope="col">Image</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Phone Number</th>
+                <th scope="col">Action</th>
             </tr>
-        <?php } ?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            <?php foreach ($contacts as $i => $contact) { ?>
 
+                <tr>
+                    <!-- <td class="d-flex justify-content-center">
+                        <input type="checkbox" name="deleteId[]" value="">
+                    </td> -->
+                    <!-- <td><input type="checkbox" class="chkCheckBoxId" value="<?php echo $account->username; ?>" name="username[]" /></td> -->
+                    <td><input type="checkbox" class="chkCheckBoxId" value="<?php echo $contact['id'] ?>" name="deleteId[]" /></td>
+                    <th scope="row"><?php echo $i + 1 ?></th>
+                    <td>
+                        <img src="<?php echo $contact['image'] ?>" height="50" width="50" />
+                    </td>
+                    <td><?php echo $contact['first_name'] ?></td>
+                    <td><?php echo $contact['last_name'] ?></td>
+                    <td><?php echo $contact['email'] ?></td>
+                    <td><?php echo $contact['phone_number'] ?></td>
+                    <td>
+                        <a href="view.php?id=<?php echo $contact['id'] ?>" type="button" class="btn btn-sm btn-warning">view</a>
+                        <a href="update.php?id=<?php echo $contact['id'] ?>" type="button" class="btn btn-sm btn-primary">Edit</a>
+
+                        <form method="post" action="delete.php" style="display:inline-block">
+                            <input type="hidden" name="id" value="<?php echo $contact['id'] ?>">
+                            <button type="submit" class="btn btn-sm btn-danger">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</form>
+
+
+
+<script type="text/javascript" src="js/jquery-1.6.2.js"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#checkBoxAll').click(function() {
+            if ($(this).is(":checked"))
+                $('.chkCheckBoxId').prop('checked', true);
+            else
+                $('.chkCheckBoxId').prop('checked', false);
+        });
+    });
+</script>
 
 <?php require './partials/footer.php' ?>
--- view.php
+++ view.php
@@ -17,21 +17,45 @@ $email = $contact['email'];
 $phone = $contact['phone_number'];
 $image = $contact['image'];
 ?>
-<h1> <strong><em><?php echo $contact['first_name'] . ' ' . $contact['last_name'] ?></em></strong></h1>
+<?php require './partials/header.php' ?>
+
+<h1 class="mx-auto d-flex justify-content-center"> <strong><em><?php echo $first_name . ' ' . $last_name ?></em></strong></h1>
 <div class="container d-flex justify-content-center">
-    <div class="row">
-        <?php if ($contact['image']) { ?>
-            <img src="<?php echo $contact['image'] ?>" width="100">
-        <?php } ?>
-    </div>
-    <div class="row">
-        <div class="col-sm"><?php echo $contact['first_name'] ?></div>
-        <div class="col-sm"><?php echo $contact['last_name'] ?></div>
+    <div class="card" style="width: 18rem;">
+        <div class="row">
+            <?php if ($contact['image']) { ?>
+                <img src="<?php echo $contact['image'] ?>" width="100">
+            <?php } ?>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-sm d-flex justify-content-between">
+                    <div class="col-3">Name: </div>
+                    <div class="col-9"><strong><?php echo $first_name . ' ' . $last_name  ?></strong></div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm d-flex justify-content-between">
+                    <div class="col-3">Phone: </div>
+                    <div class="col-9"><strong><?php echo $phone ?></strong></div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm d-flex justify-content-between">
+                    <div class="col-3">Email: </div>
+                    <div class="col-9"><strong><?php echo $email ?></strong></div>
+
+                </div>
+            </div>
+            <div class="d-flex justify-content-between mt-2">
+                <a href="index.php" class="btn btn-sm btn-secondary">Back to All Contacts</a>
+                <a href="update.php?id=<?php echo $contact['id'] ?>" type="button" class="btn btn-sm btn-primary" style="display:inline-block">Edit</a>
+            </div>
+        </div>
     </div>
 </div>
 
 
-<div class="mt-2 d-flex justify-content-end">
-    <a href="index.php">Back to All Contacts</a>
-</div>
 <?php require './partials/footer.php' ?>
